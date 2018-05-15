f0fd651db4167548ecd2cad20d8c10955576b639
张建兵 <jianbingzhang@harmonycredit.cn>
2018-05-15 14:17:26 +0800

    修改jdbc的连接池

--- src/main/java/com/nmys/story/Application.java
+++ src/main/java/com/nmys/story/Application.java
@@ -1,27 +1,19 @@
 package com.nmys.story;
 
-import com.blade.Blade;
-import com.blade.security.web.csrf.CsrfMiddleware;
-import com.blade.validator.ValidatorMiddleware;
-import com.nmys.story.init.TaleLoader;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Tale启动类
- *
- * @author biezhi
+ * Description:启动类
+ * Author:70kg
+ * Param
+ * Return
+ * Date 2018/5/10 9:38
  */
 @SpringBootApplication
 @MapperScan("com.nmys.story.mapper")
 public class Application {
-
-//    public static void main(String[] args) throws Exception {
-//        Blade blade = Blade.me();
-//        TaleLoader.init(blade);
-//        blade.use(new ValidatorMiddleware(), new CsrfMiddleware()).start(Application.class, args);
-//    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
