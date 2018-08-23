30697eb690f40fc29f35f6c7a54967ad851a5e5f
zhangjianbing <449246146@qq.com>
2018-08-23 22:52:56 +0800

    初始化sql文件

--- src/main/sql/storytemp.sql
+++ src/main/sql/storytemp.sql
@@ -1,22 +1,24 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : MyStory
-Source Server Version : 50711
-Source Host           : 47.104.189.224:3306
-Source Database       : story
+ Source Server         : mysqldatabase
+ Source Server Type    : MySQL
+ Source Server Version : 50721
+ Source Host           : localhost
+ Source Database       : storytemp
 
-Target Server Type    : MYSQL
-Target Server Version : 50711
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50721
+ File Encoding         : utf-8
 
-Date: 2018-07-27 16:52:33
+ Date: 08/23/2018 22:50:48 PM
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for t_attach
+--  Table structure for `t_attach`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_attach`;
 CREATE TABLE `t_attach` (
@@ -30,7 +32,7 @@ CREATE TABLE `t_attach` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for t_comments
+--  Table structure for `t_comments`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_comments`;
 CREATE TABLE `t_comments` (
@@ -52,7 +54,7 @@ CREATE TABLE `t_comments` (
 ) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for t_contents
+--  Table structure for `t_contents`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_contents`;
 CREATE TABLE `t_contents` (
@@ -75,10 +77,17 @@ CREATE TABLE `t_contents` (
   `allow_ping` int(10) DEFAULT NULL,
   `allow_feed` int(10) DEFAULT NULL,
   PRIMARY KEY (`cid`)
-) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for t_logs
+--  Records of `t_contents`
+-- ----------------------------
+BEGIN;
+INSERT INTO `t_contents` VALUES ('50', '欢迎光临', null, null, '1535035551', '1535035551', '> 欢迎欢迎热烈欢迎，yeah～', '1', 'post', 'publish', null, 'welcome', '默认分类', '0', '0', '1', '1', '1');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `t_logs`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_logs`;
 CREATE TABLE `t_logs` (
@@ -92,7 +101,7 @@ CREATE TABLE `t_logs` (
 ) ENGINE=InnoDB AUTO_INCREMENT=938 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for t_metas
+--  Table structure for `t_metas`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_metas`;
 CREATE TABLE `t_metas` (
@@ -104,21 +113,35 @@ CREATE TABLE `t_metas` (
   `sort` int(10) DEFAULT NULL,
   `parent` int(10) DEFAULT NULL,
   PRIMARY KEY (`mid`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for t_options
+--  Records of `t_metas`
+-- ----------------------------
+BEGIN;
+INSERT INTO `t_metas` VALUES ('40', 'welcome', 'welcome', 'tag', null, null, null), ('41', '默认分类', '默认分类', 'category', null, null, null), ('42', '农码一生为了谁', 'https://www.nmyswls.com', 'link', '', '0', null);
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `t_options`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_options`;
 CREATE TABLE `t_options` (
-  `name` varchar(255) NOT NULL,
-  `value` text,
-  `description` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL COMMENT '配置名称',
+  `value` text COMMENT '值',
+  `description` varchar(255) DEFAULT NULL COMMENT '配置描述',
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for t_relationships
+--  Records of `t_options`
+-- ----------------------------
+BEGIN;
+INSERT INTO `t_options` VALUES ('allow_install', '', '是否允许重新安装博客'), ('site_description', '本站是70KG的技术分享博客。涵盖生活故事、专注Java后端技术、SpringBoot、SpringCloud、微服务架构、高可用架构、中间件使用、系统监控等相关的研究与知识分享。', null), ('site_keywords', '农码一生,农码一生为了谁,70KG,70KG个人博客,zhangjianbing,张建兵,springboot,springcloud,java,并发编程,jvm,架构,技术分享,技术干货,系统架构,个人网站,生活随笔,开发者,编程,代码,开源,IT网站,IT技术博客', null), ('site_record', '京ICP备18029773号', null), ('site_theme', 'default', null), ('site_title', '农码一生为了谁', null), ('site_url', 'https://www.nmyswls.com', null), ('social_github', '', null), ('social_mayun', 'https://gitee.com/beijinglogic', null), ('social_twitter', 'https://gitee.com/beijinglogic', null), ('social_weibo', 'https://weibo.com/beijinglogic', null), ('social_zhihu', '', null);
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `t_relationships`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_relationships`;
 CREATE TABLE `t_relationships` (
@@ -127,7 +150,14 @@ CREATE TABLE `t_relationships` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for t_users
+--  Records of `t_relationships`
+-- ----------------------------
+BEGIN;
+INSERT INTO `t_relationships` VALUES ('50', '40'), ('50', '41');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `t_users`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_users`;
 CREATE TABLE `t_users` (
@@ -145,7 +175,14 @@ CREATE TABLE `t_users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for t_visit
+--  Records of `t_users`
+-- ----------------------------
+BEGIN;
+INSERT INTO `t_users` VALUES ('1', 'admin', '9400feec28101f42f299a0d95794a6a3', 'zhangjianbing777@gmail.com', null, '老张', null, null, null, null);
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `t_visit`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_visit`;
 CREATE TABLE `t_visit` (
@@ -154,3 +191,12 @@ CREATE TABLE `t_visit` (
   `modify_time` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `t_visit`
+-- ----------------------------
+BEGIN;
+INSERT INTO `t_visit` VALUES ('1', '2', null);
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
