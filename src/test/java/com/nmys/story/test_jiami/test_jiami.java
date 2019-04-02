1049f1bb7a016688277840b2b126ce63c6aff4ab
zhangjianbing <449246146@qq.com>
2019-04-02 21:30:04 +0800

    SpringBoot单元测试

--- src/test/java/com/nmys/story/test_jiami/test_jiami.java
+++ src/test/java/com/nmys/story/test_jiami/test_jiami.java
@@ -1,5 +1,6 @@
 package com.nmys.story.test_jiami;
 
+import com.nmys.story.Application;
 import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * Description: SpringBoot的单元测试
+ */
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 public class test_jiami {
 
     @Autowired
