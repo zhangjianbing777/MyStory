682aa9dad609287d56160e79cb07a20ee67f984b
70KG <zhangjianbing777@163.com>
2019-04-30 09:28:16 +0800

    update sha1

---
+++ src/main/java/com/nmys/story/utils/SecurityUtils.java
@@ -0,0 +1,44 @@
+package com.nmys.story.utils;
+
+import java.security.MessageDigest;
+
+/**
+ * @author Ryan
+ * time 2019/4/30
+ */
+public class SecurityUtils {
+
+    private SecurityUtils() {
+    }
+
+    /**
+     * sha1加密
+     *
+     * @param str
+     * @return
+     */
+    public static String getSha1(String str) {
+        if (str == null || str.length() == 0) {
+            return null;
+        }
+        char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        try {
+            MessageDigest mdTemp = MessageDigest.getInstance("SHA1");
+            mdTemp.update(str.getBytes("UTF-8"));
+
+            byte[] md = mdTemp.digest();
+            int j = md.length;
+            char buf[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                buf[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                buf[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(buf);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
